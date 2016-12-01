341015a2070d4e9265a7f9ac9b83cc1a68d2191f
CristianCantoro <kikkocristian@gmail.com>
2016-12-01 12:15:10 +0100

    Fix setup_user.sh changing USER/HOME to CMS_USER/CMS_USER_HOME

--- provision/setup/setup_user.sh
+++ provision/setup/setup_user.sh
@@ -6,22 +6,24 @@ IFS=$'\n\t'
 source '/tmp/provision/setup_cms/envvars.sh'
 
 # install vim and plugins
-cd "$PROVISION_DIR" && \
-    cp -r ./nvim/.vimrc "$HOME" && \
-    mkdir -p "$HOME/.vim/bundle/" && \
-    cd "$HOME/.vim/" && ln -s '../.vimrc' && \
-    ln -s '../.vimrc' 'init.vim'
+cp -r "$PROVISION_DIR/nvim/.vimrc" "$CMS_USER_HOME"
+mkdir -p "$CMS_USER_HOME/.vim/bundle/"
+[[ ! -L "$CMS_USER_HOME/.vim/.vimrc" ]] && \
+  cd "$CMS_USER_HOME/.vim/" && ln -s '../.vimrc'
+[[ ! -L "$CMS_USER_HOME/.vim/init.vim" ]] && \
+  cd "$CMS_USER_HOME/.vim/" && ln -s '../.vimrc' 'init.vim'
 echo ".vimrc installed"
 
-git clone 'https://github.com/VundleVim/Vundle.vim.git' \
-			"$HOME/.vim/bundle/Vundle.vim"
+[[ ! -d "$CMS_USER_HOME/.vim/bundle/Vundle.vim" ]] && \
+  git clone 'https://github.com/VundleVim/Vundle.vim.git' \
+            "$CMS_USER_HOME/.vim/bundle/Vundle.vim"
 nvim --headless +PluginInstall +qall 2>/dev/null
 echo "Vundle.vim and plugins installed"
 
 # install tmux and config
-cd "$PROVISION_DIR" && \
-    cp -r ./tmux/.tmux.conf "$HOME" && \
-    cp -r ./tmux/.tmux "$HOME"
+cp -r "$PROVISION_DIR/tmux/.tmux.conf" "$CMS_USER_HOME"
+[[ ! -d "$CMS_USER_HOME/.tmux" ]] && \
+	cp -r "$PROVISION_DIR/tmux/.tmux" "$CMS_USER_HOME"
 echo "tmux configs installed"
 
 exit 0
